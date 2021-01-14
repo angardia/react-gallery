ddfae1dd6658b6a0494e9395570fa3da34cb4697
angardia <37413438+angardia@users.noreply.github.com>
2021-01-14 03:12:09 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-class App extends React.Component {
+function App() {
+  const [albums, setAlbums] = useState([]);
+  const [squares, setSquares] = useState([]);
+  const [value, setValue] = useState("");
 
-  render() {
-    return (
-        <div className="App">
-          <h1>Select an album:</h1>
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/albums')
+      .then(res => res.json())
+      .then(data => setAlbums([...data]));
 
-        </div>
-    );
+
+  }, [])
+
+  function test(e) {
+    const id = e.target.selectedIndex + 1;
+    setValue(e.target.value)
+    fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${id}`)
+      .then(res => res.json())
+      .then(data => setSquares([...data]));
   }
 
+
+
+  return (
+    <div className="App">
+      <h1>Select an album:</h1>
+
+      <select value={value} onChange={(e) => test(e)} >
+        {albums.map((album, i) => {
+          return <option key={i} id={i} value={album.title}>{album.title}</option>;
+        })}
+      </select>
+
+      <div>
+        {squares.map((square, i) => {
+          return <img key={i} src={square.thumbnailUrl} alt="square" />
+        })}
+      </div>
+
+    </div>
+  );
 }
 
 export default App;
